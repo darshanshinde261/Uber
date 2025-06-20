88b3558e7b53d668cac89fa31726726587b54cd0
Darshan Shidne <darshanshinde2615@gmail.com>
2025-06-20 11:05:04 +0530

    db-connet

--- backend/app.js
+++ backend/app.js
@@ -1,6 +1,11 @@
 const express = require('express');
 const app = express();
-
+const dotenv = require('dotenv');
+const cors = require('cors');
+app.use(cors());
+dotenv.config();
+const connectDB = require("./database/database");
+connectDB();
 app.get('/',(req,res)=>{
     res.send('hello world');
 })
--- backend/server.js
+++ backend/server.js
@@ -1,8 +1,9 @@
 const app = require('./app');
 const http = require('http');
-require('dotenv').config();
+
 const port = process.env.PORT || 3000;
-console.log('port is ' + port);
+
+
 const server = http.createServer(app);
 
 server.listen(port, ()=>{
